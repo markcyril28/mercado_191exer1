93cbabe049f68b7259ebcf2fdc3dcbfda5dd445d
Mark Cyril <mercadomarkcyril28@gmail.com>
2022-03-02 11:49:48 +0800

    Added the sequence alignment function and the function for printing the sequence alignment

--- exer1/exer1.py
+++ exer1/exer1.py
@@ -42,56 +42,94 @@ def gen_matrix(x, y, match_score=3, gap_cost=2):
 
 x = "GGTTGACTA"	
 y = "TGTTACGG"
+print("Query sequences: ")
+for base in x:
+	print(base, end=" ")
+print()
+for base in y:
+	print(base, end=" ")
 
 a=gen_matrix(x,y)
 
+print("\n\nMatrix: ")
 print_matrix1(a,x,y)
 
-print("")
+print()
 
-mrows = len(x)
-ncols = len(y)
-max_num = 0
+num_traceback =[]
 x_seq = []
-bar_line = []
+bars = []
 y_seq = []
 
-for row in range(mrows):
-	for column in range(ncols):
-		if max_num <= a[row][column]:
-			max_num = a[row][column]
 
-			max_num_row = row
-			max_num_column = column
-
-			upper = a[row-1][column]
-			left = a[row][column-1]
-			diagonal = a[row-1][column-1]
-
-# The list of sequence
-x_seq.append(x[max_num_row-1])
-bar_line.append("|")
-y_seq.append(y[max_num_column-1])
-print("")
-print(x_seq)
-print(bar_line)
-print(y_seq)
-
-# Finding the max number
-print(max_num)
-
-# Printing the aligned nucleotide based on the max number
-print("")
-print(str(max_num_row) + " " + str(max_num_column))
+def seq_alignment(a,x,y):
+	# Locating the max_number in the matrix
+	mrows = len(x)
+	ncols = len(y)
+	max_num = 0
+	max_num_row = 0
+	max_num_column = 0
+	for row in range(mrows):
+		for column in range(ncols):
+			if max_num <= a[row][column]:
+				max_num = a[row][column]
+				max_num_row = row
+				max_num_column = column
 
 
-print(x[max_num_row-1])
-print(y[max_num_column-1])
+	num_traceback.append(max_num)
+	row = max_num_row
+	column = max_num_column
 
-# Comparing the Upper, Left, and Diagonal
+	# Tracing back the global alignment
+	while row > 0 and column > 0:
+		upper = a[row - 1][column]
+		left = a[row][column - 1]
+		diagonal = a[row - 1][column - 1]
+		greater_num = max(upper, left, diagonal, 0)
 
-print("")
-print(upper)
-print(left)
-print(diagonal)
+		if greater_num > 0:
+			num_traceback.append(greater_num)
 
+		if greater_num == diagonal:
+			x_seq.append(x[row-1])
+			bars.append("|")
+			y_seq.append(y[column-1])
+			row -= 1
+			column -= 1
+		elif greater_num == upper:
+			x_seq.append(x[row-1])
+			bars.append(" ")
+			y_seq.append("-")
+			row -= 1
+		elif greater_num == left:
+			x_seq.append("-")
+			bars.append(" ")
+			y_seq.append(y[column-1])
+			column -= 1
+	print("Maximun Number: ")
+	print(max_num)
+
+	# Reversing the List
+	num_traceback.reverse()
+	x_seq.reverse()
+	bars.reverse()
+	y_seq.reverse()
+
+def print_seq_alignment(num_traceback, x_seq, bars, y_seq):
+	for num in num_traceback:
+		print(num, end="	")
+	print()
+	for base in x_seq:
+		print(base, end="	")
+	print()
+	for bar in bars:
+		print(bar, end="	")
+	print()
+	for base in y_seq:
+		print(base, end="	")
+
+seq_alignment(a,x,y)
+
+print("\nSequence alignment with corresponding Traceback number: ")
+print_seq_alignment(num_traceback, x_seq, bars, y_seq)
